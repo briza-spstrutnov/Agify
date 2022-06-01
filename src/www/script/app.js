9eb945aeb9275e9c419d678d9c488ccad905816f
Adam Bříza <briza@spstrutnov.cz>
2022-06-01 13:42:07 +0200

    Vypisování dat

--- src/www/script/app.js
+++ src/www/script/app.js
@@ -1,7 +1,11 @@
 function search(name){
+    let nameResult = document.getElementById('name-result');
+    let ageResult = document.getElementById('age');
+
     fetch(`https://api.agify.io/?name=${name}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data.age);
+        nameResult.innerHTML = name;
+        ageResult.innerHTML = data.age;
     })
 }
