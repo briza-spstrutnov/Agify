fbead584e3bd1d3e4c8cbb76d4187acc02c22dbe
Adam Bříza <briza@spstrutnov.cz>
2022-06-07 13:50:25 +0200

    Gender

--- src/www/script/app.js
+++ src/www/script/app.js
@@ -1,11 +1,22 @@
 function search(name){
     let nameResult = document.getElementById('name-result');
     let ageResult = document.getElementById('age');
+    let genderResult = document.getElementById('gender');
 
     fetch(`https://api.agify.io/?name=${name}`)
     .then(response => response.json())
     .then(data => {
-        nameResult.innerHTML = name;
-        ageResult.innerHTML = data.age;
-    })
+        nameResult.innerHTML = `Jméno: ${name}`;
+        ageResult.innerHTML = `Věk: ${data.age}`;
+    });
+
+    fetch(`https://api.genderize.io/?name=${name}`)
+    .then(response => response.json())
+    .then(data => {
+        if(data.gender == 'male'){
+            genderResult.innerHTML = `Pohlaví: muž`;    
+        }else{
+            genderResult.innerHTML = `Pohlaví: žena`
+        }
+    });
 }
